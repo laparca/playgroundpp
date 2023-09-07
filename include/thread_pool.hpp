0cf5ae7a3af9a498b64ff54fa7813105ae5accd7
Samuel R. Sevilla <laparca@laparca.es>
2023-09-07 09:08:04 +0000

    feat: thread pool utility
    This thread pool help in the creation and management of multiple
    threads that should do the same work.

---
+++ include/thread_pool.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <thread>
+#include <type_traits>
+
+namespace laparca {
+
+template<typename F>
+class thread_pool {
+public:
+    thread_pool(size_t pool_size, F f)
+    : pool_(pool_size)
+    {
+        for (auto& element : pool_) {
+            element = std::make_unique<std::thread>(f);
+        }
+    }
+
+    void join() {
+        for (auto& element : pool_) {
+            element->join();
+        }
+    }
+private:
+    std::vector<std::unique_ptr<std::thread>> pool_;
+};
+
+template<typename F>
+thread_pool(size_t, F) -> thread_pool<std::decay_t<F>>;
+}
